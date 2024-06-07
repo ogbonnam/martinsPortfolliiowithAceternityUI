50f8c06dd351c0b1ca9068d7cb642a03068db7ab
unknown <georgeenueshike@gmail.com>
2024-06-07 09:16:16 +0100

    fixing deployment issues

--- app/page.tsx
+++ app/page.tsx
@@ -10,29 +10,7 @@ export default function Home() {
       <div>
         <p>Logo</p>
       </div>
-      <HeroHighlight className="items-center justify-between">
-        <motion.h1
-          initial={{
-            opacity: 0,
-            y: 20,
-          }}
-          animate={{
-            opacity: 1,
-            y: [20, -5, 0],
-          }}
-          transition={{
-            duration: 0.5,
-            ease: [0.4, 0.0, 0.2, 1],
-          }}
-          className="text-2xl px-4 md:text-4xl lg:text-5xl font-bold text-neutral-700 dark:text-white max-w-4xl leading-relaxed lg:leading-snug text-center mx-auto "
-        >
-          Building Modern Web Applications should'nt be hard!. Turn your ideas
-          into {" "}
-          <Highlight className="text-black dark:text-white">
-            stunning websites using latest technologies.
-          </Highlight>
-        </motion.h1>
-      </HeroHighlight>
+      
       <div className="bg-white text-black text-center justify-between pt-28 pb-28">
         <div className="flex flex-wrap w-4/5 mx-auto gap-8">
           <SingleCard />
